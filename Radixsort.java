9d2640c43d89dd0116222c633cfb0f7aec100781
CubeCrafterTV <timonk04@gmail.com>
2022-05-14 01:14:52 +0200

    weiter dran gearbeitet

--- Radixsort.java
+++ Radixsort.java
@@ -10,10 +10,14 @@ public class Radixsort{
             System.out.println("Error: You have to enter at least one number, and can only enter numbers");
             return;
         }
-		
-		System.out.println("Array:        " + Arrays.toString(data));
+		int[] data1 = copyArray(data);
+		System.out.println("lsd Array:        " + Arrays.toString(data));
 		lsdRadix(data);
-		System.out.println("Sorted Array: " + Arrays.toString(data));
+		System.out.println("lsd Sorted Array: " + Arrays.toString(data));
+		System.out.println("");
+		System.out.println("msd Array:        " + Arrays.toString(data1));
+		msdRadix(data1);
+		System.out.println("msd Sorted Array: " + Arrays.toString(data1));
 	}
 	public static void sortByByte(int[] input, int l, int r, int b){
 		//System.out.println("CountingSort Iteration++");
@@ -42,7 +46,44 @@ public class Radixsort{
 			sortByByte(data, 0, data.length-1,i);
 		}
 	}
-	
+	public static void msdRadix(int[] data){
+		msdRadix(data,0,data.length-1,3);
+	}
+	public static void msdRadix(int[] data, int l, int r, int b){
+		if(r-l+1 <= 32) inSort(data,l,r);
+		sortByByte(data,l,r,b);
+		int[] C = new int[257];
+		for(int i = l+1; i <= r; i++){
+			if(((data[i] >> (8*b)) & 0xFF) == ((data[i-1] >> (8*b)) & 0xFF)){
+				C[(data[i] >> (8*b)) & 0xFF]++;
+			} else {
+				C[(data[i] >> (8*b)) & 0xFF] = 1;
+			}
+		}
+		for(int i = 0; i < C.length-1; i++){
+			msdRadix(data, l+C[i],l+C[i+1]-1 ,b-1);
+		}
+	}
+	public static void inSort(int[] data, int l, int r){
+		
+		int temp;
+		for (int i = l+1; i <= r; i++) {
+			temp = data[i];
+			int j = i;
+			while (j > l && data[j - 1] > temp) {
+				data[j] = data[j - 1];
+				j--;
+			}
+			data[j] = temp;
+		}
+	}
+	public static int[] copyArray(int[] data){
+		int[] result = new int[data.length];
+		for(int i = 0; i < data.length; i++){
+			result[i] = data[i];
+		}
+		return result;
+	}
 	public static int[] scanIntArray(){
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> inputs = new ArrayList<String>();
