15c18586489891d60e969dd0c5c4c3157067ce67
CubeCrafterTV <timonk04@gmail.com>
2022-07-03 20:53:44 +0200

    tmp funktioinsfähig bei graph 1

--- FordFulkerson.java
+++ FordFulkerson.java
@@ -20,6 +20,7 @@ Graph{
 	String toString()
 }
 */
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.io.IOException;
@@ -49,44 +50,112 @@ public class FordFulkerson{
 			System.out.println("Gesuchte Knoten nicht im Graphen (zu groß oder klein)");
 			return;
 		}
-		ArrayList<Integer> npath = bfs(graph,src,dst);
-		if(npath == null){
-			System.out.println("Kein weg gefunden");
+		
+		int result = fordFulkerson(graph,src,dst);
+		if(result==-1){
+			System.out.println("Kein Weg gefunden");
 			return;
 		}
-		System.out.println(npath.toString());
+		System.out.println("Der gesuchte Flow ist:" + result);
 		
 	}
-	public static void fordFulkerson(WeightGraph graph, int src, int dst){
-		ArrayList<Integer> augmentedPath = bfs(graph, src, dst);
+	public static int fordFulkerson(WeightGraph graph, int src, int dst){
+		int[] flow = new int[graph.getNumEdges()];
+		int dstFlow = 0;
+		int[] transitionTable = new int[graph.getNumNodes()];
+		for(int i = 1; i < transitionTable.length; i++){
+			transitionTable[i] = graph.getNode(i-1).getEdgeList().size() + transitionTable[i-1];
+		}
+		
+		ArrayList<Integer> augmentedPath = bfs(graph, src, dst, flow, transitionTable);
+		//boolean loopEntered = false;
+		while(augmentedPath != null){
+			//loopEntered = true;
+			System.out.println(augmentedPath.size() + " Pfad:       " + augmentedPath.toString());
+			System.out.println(transitionTable.length + " TransTable: " + Arrays.toString(transitionTable));
+			System.out.println(flow.length + " Flow:       " + Arrays.toString(flow));
+			//TODO bfs muss bei suchen eines Pfades den flow berücksichtigen
+			int bottleneckCapacity = Integer.MAX_VALUE;
+			//find out bottleneck
+			for(int i = 0; i < augmentedPath.size()-1; i++){
+				//suche aktuelle Kante
+				LinkedList<WeightEdge> edges = graph.getNode(augmentedPath.get(i)).getEdgeList();
+				WeightEdge edge = null;
+				int edgeNum = 0;
+				for(int j = 0; j < edges.size(); j++){
+					if(edges.get(j).getDst().getID() == augmentedPath.get(i+1)){
+						edge = edges.get(j);
+						edgeNum = j;
+					}
+				}
+				if(edge == null){
+					System.out.println("Irgendein komischer Fehler ist passiert");
+					return -1;
+				}
+				//System.out.println(" Cap:       " + edge.getWeight());
+				//System.out.println(" Flow:       " + flow[transitionTable[i]+edgeNum] + "  edgedNum: " + edgeNum);
+				int currentBottleneck = edge.getWeight()-flow[transitionTable[augmentedPath.get(i)]+edgeNum];
+				if(currentBottleneck < bottleneckCapacity) bottleneckCapacity = currentBottleneck;
+			}
+			//adjusting path with new bottleneckCapacity
+			for(int i = 0; i < augmentedPath.size()-1; i++){
+				//suche aktuelle Kante
+				LinkedList<WeightEdge> edges = graph.getNode(augmentedPath.get(i)).getEdgeList();
+				WeightEdge edge = null;
+				int edgeNum = -1;
+				for(int j = 0; j < edges.size(); j++){
+					if(edges.get(j).getDst().getID() == augmentedPath.get(i+1)){
+						edge = edges.get(j);
+						edgeNum = j;
+						break;
+					}
+				}
+				if(edge == null){
+					System.out.println("Irgendein komischer Fehler ist passiert");
+					return -1;
+				}
+				flow[transitionTable[i]+edgeNum]+=bottleneckCapacity;
+			}
+			dstFlow+=bottleneckCapacity;
+			//System.out.println(transitionTable.length + " TransTable: " + Arrays.toString(transitionTable));
+			//System.out.println(flow.length + " Flow:       " + Arrays.toString(flow));
+			
+			augmentedPath = bfs(graph, src, dst, flow, transitionTable);
+			
+			
+		}
+		//if(!loopEntered)return -1;
+		return dstFlow;
 	}
-	public static ArrayList<Integer> bfs(WeightGraph graph, int src, int dst){
-		int[][] pre = new int[sizeOf(graph)][2];
+	public static ArrayList<Integer> bfs(WeightGraph graph, int src, int dst, int[] flow, int[] transitionTable){
+		int[] pre = new int[sizeOf(graph)];
 		ArrayList<Integer> handled = new ArrayList<>();
 		for(int i = 0; i < sizeOf(graph);i++){
 			graph.getNode(i).setMark(false);
-			pre[i][1] = -1;
+			pre[i] = -1;
 		}
 		handled.add(src);
 		graph.getNode(src).setMark(true);
-		while(pre[dst][1] == -1 && !handled.isEmpty() ){
+		while(pre[dst] == -1 && !handled.isEmpty() ){
 			int indexCurrentNode = handled.size()-1;
 			LinkedList<WeightEdge> edges = graph.getNode(handled.get(indexCurrentNode)).getEdgeList();
 			for(int i = 0; i < edges.size(); i++){
-				if(!edges.get(i).getDst().isMarked()){
-					edges.get(i).getDst().setMark(true);
-					pre[edges.get(i).getDst().getID()][1] = handled.get(indexCurrentNode);
-					handled.add(edges.get(i).getDst().getID());
+				//Einzige veränderte Zeile für int[] flow und int[]transitionTable
+				WeightNode nextNode = edges.get(i).getDst();
+				if(!nextNode.isMarked() && flow[transitionTable[handled.get(indexCurrentNode)]+i] < edges.get(i).getWeight()){
+					nextNode.setMark(true);
+					pre[nextNode.getID()] = handled.get(indexCurrentNode);
+					handled.add(nextNode.getID());
 				}
 			}
 			handled.remove(indexCurrentNode);
 		}
-		if(pre[dst][1] == -1)return null;
+		if(pre[dst] == -1)return null;
 		ArrayList<Integer> path = new ArrayList<>();
 		int node = dst;
 		path.add(dst);
 		while(node != src){
-			node = pre[node][1];
+			node = pre[node];
 			path.add(0,node);
 			
 		} 
